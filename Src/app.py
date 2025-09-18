1b2271ced5c85703412563c9caff9602aeba1eb7
Wajid <errormrcoder@example.com>
2025-09-18 23:58:52 +0500

    Connect db

--- Src/app.py
+++ Src/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template as render, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy as sql
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///biomatric.db'
+
+db = sql(app)
 
 @app.route('/')
 def Home():
